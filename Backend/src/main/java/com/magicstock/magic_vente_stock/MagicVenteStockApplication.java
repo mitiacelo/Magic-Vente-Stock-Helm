6cdb05fde2e518d6df6635c8dcba8abc923f1835
al <rabekototojo@mgail.com>
2026-06-06 10:06:49 +0300

    Creation du backend

---
+++ Backend/src/main/java/com/magicstock/magic_vente_stock/MagicVenteStockApplication.java
@@ -0,0 +1,13 @@
+package com.magicstock.magic_vente_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicVenteStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicVenteStockApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/magicstock/magic_vente_stock/MagicVenteStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicstock.magic_vente_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicVenteStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
